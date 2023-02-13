33311e49a84770c511d6538b9fee92e573406df3
farkasf <120285330+farkasf@users.noreply.github.com>
2023-02-13 02:12:22 +0100

    Header file added. cs% convertions added.

---
+++ includes/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 01:58:59 by ffarkas           #+#    #+#             */
+/*   Updated: 2023/02/13 02:08:14 by ffarkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "./libft/libft.h"
+
+int	ft_printf(const char *str, ...);
+int	ft_get_param(va_list args, const char format);
+
+int	ft_print_c(va_list *args);
+int	ft_print_s(va_list *args);
+int	ft_percent();
+
+#endif
---
+++ sources/ft_csp.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_csp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 01:53:23 by ffarkas           #+#    #+#             */
+/*   Updated: 2023/02/13 02:07:03 by ffarkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_print_c(va_list *args)
+{
+	char	c;
+
+	c = va_arg(*args, int);
+	ft_putchar_fd(c, 1);
+	return (1);
+}
+
+int	ft_print_s(va_list *args)
+{
+	char	*str;
+	int	count;
+
+	count = 1;
+	str = va_arg(*args, char *);
+	if (str == 0)
+		str = "(null)";
+	ft_putstr_fd(str, 1);
+	count = ft_strlen(str);
+	return (count);
+}
+
+int	ft_percent()
+{
+	ft_putchar_fd('%', 1);
+	return (1);
+}
