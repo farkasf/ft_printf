9e5a1c8a712af15b56faf26eb1e7abfc50305bd7
farkasf <120285330+farkasf@users.noreply.github.com>
2023-02-13 21:46:39 +0100

    Header file: updated /cs: order of the functions changed /dixX: syntax edited

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 01:58:59 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/02/13 21:21:32 by ffarkas          ###   ########.fr       */
+/*   Updated: 2023/02/13 21:43:20 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,7 @@ int	ft_print_di(va_list *args);
 int	ft_puthex(unsigned int num, const char format);
 int	ft_hexlen(unsigned int num);
 int	ft_print_xX(va_list *args, const char format);
+int	ft_print_u(va_list *args);
+char	*ft_uitoa(unsigned int num);
 
 #endif
--- sources/ft_dixX.c
+++ sources/ft_dixX.c
@@ -6,7 +6,7 @@
 /*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 02:20:22 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/02/13 21:19:14 by ffarkas          ###   ########.fr       */
+/*   Updated: 2023/02/13 21:31:29 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int	ft_puthex(unsigned int num, const char format)
 	}
 }
 
-int	ft_hexlen(unsigned int num)
+int	ft_numlen(unsigned int num)
 {
 	int	len;
 
@@ -73,5 +73,5 @@ int	ft_print_xX(va_list *args, const char format)
 	}
 	else
 		ft_puthex(num, format);
-	return (ft_hexlen(num));
+	return (ft_numlen(num));
 }
