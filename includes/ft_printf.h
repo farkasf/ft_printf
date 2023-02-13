b7b66f96062829d0e75082249c88d723d0aad2f9
farkasf <120285330+farkasf@users.noreply.github.com>
2023-02-13 21:22:15 +0100

    Header file updated for dixX

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 01:58:59 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/02/13 17:36:15 by ffarkas          ###   ########.fr       */
+/*   Updated: 2023/02/13 21:21:32 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,9 @@ int	ft_get_param(va_list args, const char format);
 int	ft_print_c(va_list *args);
 int	ft_print_s(va_list *args);
 int	ft_print_percent(void);
+int	ft_print_di(va_list *args);
+int	ft_puthex(unsigned int num, const char format);
+int	ft_hexlen(unsigned int num);
+int	ft_print_xX(va_list *args, const char format);
 
 #endif
