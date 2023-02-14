2a46a6b46ae60a1fed8f1d345792255e1ca97c2e
farkasf <120285330+farkasf@users.noreply.github.com>
2023-02-14 15:33:34 +0100

    dixX: forbidden char name Norm error fixed

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print.c                                         :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 01:20:10 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/02/13 23:12:47 by ffarkas          ###   ########.fr       */
+/*   Updated: 2023/02/14 15:32:28 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	ft_get_param(va_list *args, const char format)
 	else if (format == 'u')
 		length += ft_print_u(args);
 	else if (format == 'x' || format == 'X')
-		length += ft_print_xX(args, format);
+		length += ft_print_x(args, format);
 	else if (format == '%')
 		length += ft_print_percent();
 	return (length);
