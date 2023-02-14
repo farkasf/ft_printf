c72c93515e99d75f0863111a979d6e242e74a70a
Filip Farkas <ffarkas@c2r1s3.42prague.com>
2023-02-14 15:28:36 +0100

    Header file: Norm errors fixed

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 01:58:59 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/02/13 23:15:42 by ffarkas          ###   ########.fr       */
+/*   Updated: 2023/02/14 15:25:18 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,22 @@
 # include <stdarg.h>
 # include "./libft/libft.h"
 
-int	ft_printf(const char *str, ...);
-int	ft_get_param(va_list *args, const char format);
-
-int	ft_print_c(va_list *args);
-int	ft_print_s(va_list *args);
-int	ft_print_percent(void);
-int	ft_print_di(va_list *args);
-void	ft_puthex(unsigned int num, const char format);
-int	ft_hexlen(unsigned int num);
-int	ft_print_xX(va_list *args, const char format);
-int	ft_numlen(unsigned int num);
-char	*ft_uitoa(unsigned int num);
-int	ft_print_u(va_list *args);
-int	ft_ptrlen(unsigned long long ptr);
 void	ft_putptr(unsigned long long int ptr);
-int	ft_print_p(va_list *args);
+void	ft_puthex(unsigned int num, const char format);
+
+char	*ft_uitoa(unsigned int num);
+
+int		ft_printf(const char *str, ...);
+int		ft_get_param(va_list *args, const char format);
+int		ft_print_c(va_list *args);
+int		ft_print_s(va_list *args);
+int		ft_print_percent(void);
+int		ft_print_di(va_list *args);
+int		ft_hexlen(unsigned int num);
+int		ft_print_xX(va_list *args, const char format);
+int		ft_numlen(unsigned int num);
+int		ft_print_u(va_list *args);
+int		ft_ptrlen(unsigned long long ptr);
+int		ft_print_p(va_list *args);
 
 #endif
